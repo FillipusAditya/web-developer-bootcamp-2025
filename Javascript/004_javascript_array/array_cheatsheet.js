a7e105538c1844a8513521255220688e008d00e0
FillipusAditya <fillipusadityanugroho@gmail.com>
2025-07-04 12:39:08 +0700

    first commit

---
+++ Javascript/004_javascript_array/array_cheatsheet.js
@@ -0,0 +1,17 @@
+// Making an array:
+const colors = ["red", "orange", "yellow"];
+
+// Arrays are indexed like strings:
+colors[0]; // "red"
+
+// They have a length:
+colors.length; //3
+
+// Important array methods:
+// push(value) - adds value to the END of an array
+// pop() - removes and returns last value in array
+
+// unshift(val) - adds value to START of an array
+// shift() - removes and returns first element in an array
+
+// https://www.canva.com/design/DAECFJDBnCc/4vEl42xfgTTZMwLF0bqEUQ/view?utm_content=DAECFJDBnCc&utm_campaign=designshare&utm_medium=link&utm_source=editor
